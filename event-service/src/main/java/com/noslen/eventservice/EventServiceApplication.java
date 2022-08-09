cc8208a1f80e98255f490edea0f485951916e3de
Nelson Johns <nelsonjohns@Nelsons-iMac.askey.com>
2022-08-09 00:33:34 -0400

    revert to last

--- event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
@@ -3,8 +3,10 @@ package com.noslen.eventservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 @EnableDiscoveryClient
 public class EventServiceApplication {
 
