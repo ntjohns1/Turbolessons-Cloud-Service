096a1fa20539cd5d2217b42a365b5a886665a042
Nelson Johns <nelsonjohns@Nelsons-iMac.askey.com>
2022-08-11 15:47:25 -0400

    Configuring Docker images

--- lsa-cloud-service/authorization-server/src/main/java/com/noslen/auth/AuthorizationServerApp.java
+++ lsa-cloud-service/authorization-server/src/main/java/com/noslen/auth/AuthorizationServerApp.java
@@ -4,20 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 
-import com.noslen.auth.config.KeycloakServerProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
 
 @SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
-@EnableConfigurationProperties({ KeycloakServerProperties.class })
 @EnableEurekaServer
 //@ConfigurationProperties(prefix = "spring.liquibase", ignoreUnknownFields = false)
 public class AuthorizationServerApp {
@@ -28,17 +23,5 @@ public class AuthorizationServerApp {
 		SpringApplication.run(AuthorizationServerApp.class, args);
 	}
 
-	@Bean
-	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
-			KeycloakServerProperties keycloakServerProperties) {
-
-		return (evt) -> {
-
-			Integer port = serverProperties.getPort();
-			String keycloakContextPath = keycloakServerProperties.getContextPath();
-
-			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
-		};
-	}
 
 }
--- lsa-cloud-service/event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
+++ lsa-cloud-service/event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
@@ -2,10 +2,10 @@ package com.noslen.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class EventServiceApplication {
 
 	public static void main(String[] args) {
