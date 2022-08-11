a452b894f6a547a78ccef43c5f6f6383d0bbf85e
Nelson Johns <nelsonjohns@Nelsons-iMac.askey.com>
2022-08-11 17:41:53 -0400

    fixed missing container module

---
+++ lsa-cloud-service/gateway-service/src/test/java/com/noslen/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noslen.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
