ca94d6599e07f2179e3d05ab0213a3da4495ccb2
ntjohns1 <noslen@Nelsons-Air.askey.com>
2022-08-13 06:57:46 -0400

    reverted back to embedded keycloak

--- lsa-cloud-service/authorization-server/src/main/java/com/noslen/auth/AuthorizationServerApp.java
+++ lsa-cloud-service/authorization-server/src/main/java/com/noslen/auth/AuthorizationServerApp.java
@@ -4,17 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-
-
+import com.noslen.auth.config.KeycloakServerProperties;
 
 @SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
-@EnableEurekaServer
-//@ConfigurationProperties(prefix = "spring.liquibase", ignoreUnknownFields = false)
+@EnableConfigurationProperties({ KeycloakServerProperties.class })
 public class AuthorizationServerApp {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AuthorizationServerApp.class);
@@ -23,5 +23,17 @@ public class AuthorizationServerApp {
 		SpringApplication.run(AuthorizationServerApp.class, args);
 	}
 
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
+			KeycloakServerProperties keycloakServerProperties) {
+
+		return (evt) -> {
+
+			Integer port = serverProperties.getPort();
+			String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
+		};
+	}
 
 }
