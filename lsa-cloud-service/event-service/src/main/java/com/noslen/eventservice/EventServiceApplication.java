df20c1806fe454a1a88af2f4dcfe7e9a8e546bc6
ntjohns1 <noslen@Nelsons-Air.askey.com>
2022-08-14 19:26:55 -0400

    fixed Dockerfile for auth server

--- lsa-cloud-service/event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
+++ lsa-cloud-service/event-service/src/main/java/com/noslen/eventservice/EventServiceApplication.java
@@ -2,10 +2,13 @@ package com.noslen.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class EventServiceApplication {
 
 	public static void main(String[] args) {
