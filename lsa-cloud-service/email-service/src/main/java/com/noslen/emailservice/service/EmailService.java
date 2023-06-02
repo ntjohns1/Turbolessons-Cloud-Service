d674da6a27fd7d56c8f63358d6b8bfeb4c63eaa0
ntjohns1 <nelsontjohns@gmail.com>
2023-06-02 00:18:16 -0400

    application context and repository tests passing

--- lsa-cloud-service/email-service/src/main/java/com/noslen/emailservice/service/EmailService.java
+++ lsa-cloud-service/email-service/src/main/java/com/noslen/emailservice/service/EmailService.java
@@ -1,13 +1,4 @@
 package com.noslen.emailservice.service;
 
-import javax.mail.MessagingException;
-import java.io.IOException;
-import java.util.Map;
-
 public interface EmailService {
-    void sendHTMLMessage(String to,
-                                           String subject,
-                                           Map<String, Object> templateModel) 
-            throws IOException, MessagingException;
-
 }
