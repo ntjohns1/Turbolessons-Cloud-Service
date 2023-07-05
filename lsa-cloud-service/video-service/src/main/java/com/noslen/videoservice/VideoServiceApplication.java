cf6264b479e664c52dc8d445b1d9a63b053cf125
ntjohns1 <nelsontjohns@gmail.com>
2023-07-04 22:16:18 -0400

    working state for config and docker-compose

--- lsa-cloud-service/video-service/src/main/java/com/noslen/videoservice/VideoServiceApplication.java
+++ lsa-cloud-service/video-service/src/main/java/com/noslen/videoservice/VideoServiceApplication.java
@@ -2,7 +2,9 @@ package com.noslen.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VideoServiceApplication {
 
