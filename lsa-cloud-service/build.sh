15a04f31093c80e120f8cd60a8d99f00b666cc38
ntjohns1 <nelsontjohns@gmail.com>
2023-07-11 13:56:31 -0400

    staging for deploy 2

--- lsa-cloud-service/build.sh
+++ lsa-cloud-service/build.sh
@@ -5,40 +5,40 @@ echo "Performing a clean Maven build"
 
 echo "Building the config server"
 cd lsa-config-service
-docker build --tag com-noslen-lsa-config-service .
+docker build -t noslenj/lsa-config-service:com-noslen-lsa-config-service .
 cd ..
 
 echo "Building the service registry"
 cd service-registry
-docker build --tag com-noslen-service-registry .
+docker build -t noslenj/service-registry:com-noslen-service-registry .
 cd ..
 
 echo "Building admin-service"
 cd admin-service
-docker build --tag com-noslen-admin-service .
+docker build -t noslenj/admin-service:com-noslen-admin-service .
 cd ..
 
 echo "Building email-service"
 cd email-service
-docker build --tag com-noslen-email-service .
+docker build -t noslenj/email-service:com-noslen-email-service .
 cd ..
 
 echo "Building event-service"
 cd event-service
-docker build --tag com-noslen-event-service .
+docker build -t noslenj/event-service:com-noslen-event-service .
 cd ..
 
 echo "Building message-service"
 cd message-service
-docker build --tag com-noslen-message-service .
+docker build -t noslenj/message-service:com-noslen-message-service .
 cd ..
 
 echo "Building video-service"
 cd video-service
-docker build --tag com-noslen-video-service .
+docker build -t noslenj/video-service:com-noslen-video-service .
 cd ..
 
 echo "Building api-gateway"
 cd api-gateway
-docker build --tag com-noslen-api-gateway .
-cd ..
+docker build -t noslenj/api-gateway:com-noslen-api-gateway .
+
