88add8718d53164b38ffa922689a0c3a7ca21a92
ntjohns1 <nelsontjohns@gmail.com>
2023-07-15 18:10:56 -0400

    Updates to message-service WebSocketConfig

--- lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/WebSocketConfig.java
+++ lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/WebSocketConfig.java
@@ -39,13 +39,15 @@ class WebSocketConfig {
         SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
         mapping.setOrder(10);
         Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/ws/messages", wsh);
-        map.put("/ws/messages/{userId}", wsh);
+//        ## Uncomment for Local Dev
+//        map.put("w/ws/messages", wsh);
+//        map.put("/ws/messages/{userId}", wsh);
+        map.put("wss://www.turbolessons.com/ws/messages", wsh);
+        map.put("wss://www.turbolessons.com/ws/messages/ws/messages/{userId}", wsh);
         mapping.setUrlMap(map);
         mapping.setCorsConfigurations(Collections.singletonMap("*", new CorsConfiguration().applyPermitDefaultValues()));
         return mapping;
     }
-
 
     @Bean
     WebSocketHandlerAdapter webSocketHandlerAdapter() {
