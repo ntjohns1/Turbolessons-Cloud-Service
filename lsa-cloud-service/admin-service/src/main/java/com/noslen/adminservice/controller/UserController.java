b0197878bbae629ad24e081aeb8a0da7083ab8b8
ntjohns1 <nelsontjohns@gmail.com>
2023-08-13 22:19:33 -0400

    test log for ci setup

--- lsa-cloud-service/admin-service/src/main/java/com/noslen/adminservice/controller/UserController.java
+++ lsa-cloud-service/admin-service/src/main/java/com/noslen/adminservice/controller/UserController.java
@@ -41,6 +41,7 @@ public class UserController {
         userService.updateUser(id, dto);
     }
 
+    // test comment
     @DeleteMapping("/api/users/{id}")
     public void deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
