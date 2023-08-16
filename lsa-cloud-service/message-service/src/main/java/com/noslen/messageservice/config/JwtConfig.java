f5b14e83c58d36136575848918edb6355e7aeacc
ntjohns1 <nelsontjohns@gmail.com>
2023-08-16 00:02:51 -0400

    comment out JwtConfig

--- lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/JwtConfig.java
+++ lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/JwtConfig.java
@@ -1,19 +1,19 @@
-package com.noslen.messageservice.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-@Configuration
-public class JwtConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkSetUri;
-
-    @Bean
-    public NimbusJwtDecoder jwtDecoder() {
-        return (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(jwkSetUri);
-    }
-}
+//package com.noslen.messageservice.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.jwt.JwtDecoders;
+//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+//
+//@Configuration
+//public class JwtConfig {
+//
+//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+//    private String jwkSetUri;
+//
+//    @Bean
+//    public NimbusJwtDecoder jwtDecoder() {
+//        return (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(jwkSetUri);
+//    }
+//}
