32402138cad1bccf49df97b8d1c82c7ac6ebd11a
ntjohns1 <nelsontjohns@gmail.com>
2023-08-16 14:28:51 -0400

    try manually creating JwtDecoder bean again

--- lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/JwtConfig.java
+++ lsa-cloud-service/message-service/src/main/java/com/noslen/messageservice/config/JwtConfig.java
@@ -1,19 +1,23 @@
-//package com.noslen.messageservice.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.oauth2.jwt.JwtDecoders;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//
-//@Configuration
-//public class JwtConfig {
-//
-//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-//    private String jwkSetUri;
-//
-//    @Bean
-//    public NimbusJwtDecoder jwtDecoder() {
-//        return (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(jwkSetUri);
-//    }
-//}
+package com.noslen.messageservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+
+    @Bean
+    public JwtDecoder jwtDecoder() throws MalformedURLException {
+        URL jwkSetUri = new URL(new URL(issuer), "/v1/keys");
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri.toString()).build();
+    }
+}
