eb8e0ea82ccb63e9ecab5f224e8a2e23878881df
ntjohns1 <nelsontjohns@gmail.com>
2024-06-18 13:38:38 -0400

    rename root directory

---
+++ turbolessons/config-service/src/main/java/com/noslen/lsaconfigservice/ConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.noslen.turbolessonsconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.noslen.turbolessonsconfigservice.ConfigServiceApplication.class, args);
+	}
+
+}
---
+++ turbolessons/config-service/src/test/java/com/noslen/lsaconfigservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noslen.turbolessonsconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
