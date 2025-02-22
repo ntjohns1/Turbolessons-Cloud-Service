3f7354150aa8042a058e2e009d87c15a96d09c7a
ntjohns1 <nelsontjohns@gmail.com>
2025-02-22 14:41:50 -0500

    implemented UsageRecord controller layer classes

--- turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/controller/usagerecord/UsageRecordEndpointConfig.java
+++ turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/controller/usagerecord/UsageRecordEndpointConfig.java
@@ -1,4 +1,25 @@
 package com.turbolessons.paymentservice.controller.usagerecord;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class UsageRecordEndpointConfig {
+
+    private final UsageRecordHandler handler;
+
+    public UsageRecordEndpointConfig(UsageRecordHandler handler) {
+        this.handler = handler;
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> usageRecordRoutes() {
+        return route((POST("/api/payments/usage_record")),
+                     handler::create);
+    }
 }
--- turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/controller/usagerecord/UsageRecordHandler.java
+++ turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/controller/usagerecord/UsageRecordHandler.java
@@ -1,4 +1,9 @@
 package com.turbolessons.paymentservice.controller.usagerecord;
 
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
 public interface UsageRecordHandler {
+    Mono<ServerResponse> create(ServerRequest r);
 }
