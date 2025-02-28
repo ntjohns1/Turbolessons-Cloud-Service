5a67bd7a84e31d37ffe766c4fa8e7168fb5d15f9
ntjohns1 <nelsontjohns@gmail.com>
2025-02-28 10:33:33 -0500

    update naming convention in MeterData fields to match Stripe objects

--- turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/dto/MeterData.java
+++ turbolessons/payment-service/src/main/java/com/turbolessons/paymentservice/dto/MeterData.java
@@ -1,4 +1,4 @@
 package com.turbolessons.paymentservice.dto;
 
-public record MeterData(String id, String displayName, String eventName) {
+public record MeterData(String id, String display_name, String event_name) {
 }
