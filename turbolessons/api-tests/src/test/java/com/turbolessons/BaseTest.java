33d436a674dcd3ce714fdbc3063627898e71851a
ntjohns1 <nelsontjohns@gmail.com>
2025-03-24 20:14:58 -0400

    snapshot before refactor to use Spring Boot

--- turbolessons/api-tests/src/test/java/com/turbolessons/BaseTest.java
+++ turbolessons/api-tests/src/test/java/com/turbolessons/BaseTest.java
@@ -4,24 +4,30 @@ package com.turbolessons;
 import com.turbolessons.config.AppConfig;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class BaseTest {
+public abstract class BaseTest {
 
     @BeforeAll
     public static void setUp() {
         RestAssured.baseURI = AppConfig.getBaseUrl();
+        String accessToken = obtainAccessToken(); // Use client credentials flow here
+        RestAssured.requestSpecification = new RequestSpecBuilder().addHeader("Authorization",
+                                                                              "Bearer " + accessToken)
+                .setContentType(ContentType.JSON)
+                .build();
 
-        RestAssured.filters(
-                new RequestLoggingFilter(),
-                new ResponseLoggingFilter(),
-                new AllureRestAssured()
-        );
+        RestAssured.filters(new RequestLoggingFilter(),
+                            new ResponseLoggingFilter(),
+                            new AllureRestAssured());
+    }
+
+    private static String obtainAccessToken() {
+        // send a request to Okta's token endpoint and returning the token.
+        return "generated-token-string";
     }
 }
